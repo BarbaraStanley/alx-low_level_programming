d645e12894e719eba227c85630076fbb2fa0eecb
BarbaraStanley <barbeaboo@gmail.com>
2022-03-11 10:54:31 -0800

    Task 6 Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("size of an integer:%ld\n", sizeof(int));
+	printf("size of a char:%ld\n", sizeof(char));
+	printf("size of long:%ld\n", sizeof(long));
+	printf("size of long long int:%ld\n", sizeof(long long));
+	printf("sixe of float:%ld\n", sizeof(float));
+	return (0);
+}
+	
