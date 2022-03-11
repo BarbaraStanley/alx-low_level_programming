f8ba1874d74c11d2acde1db3c3079201639d34d7
BarbaraStanley <barbeaboo@gmail.com>
2022-03-11 09:44:38 -0800

    Task 4 Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char Barb[] = "Programming is like building a multilingual puzzle ";
+	char Barb[] = "\"Programming is like building a multilingual puzzle";
 	puts(Barb);
 	return (0);
 }
