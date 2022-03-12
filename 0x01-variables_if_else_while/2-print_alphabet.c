122cf9a4a569857044e5cbcaaf78468df6f0815a
BarbaraStanley <barbeaboo@gmail.com>
2022-03-11 20:57:06 -0800

    Task 2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point, print the alphabet in lower case
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 97;
+
+	while(i<=200)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
