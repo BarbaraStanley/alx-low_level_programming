e6c061b249dccc549a6c662fc6bc6fff4979776a
BarbaraStanley <barbeaboo@gmail.com>
2022-03-11 19:55:12 -0800

    Task 0 positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/*
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if(n>0)
+        {
+                printf("%d is positive", n);
+        } else if(n=0)
+                printf("%d is 0", n);
+        else
+                printf("%d is negative", n);
+        return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Learning variables and loops in C
