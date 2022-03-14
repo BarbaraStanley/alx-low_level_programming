3700d9ca03d7d830908a75e483c7287aa836d16b
BarbaraStanley <barbeaboo@gmail.com>
2022-03-14 05:06:58 -0700

    Task 6 numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point, print all single digit numbers of base 10 using putchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+        while (i < 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
