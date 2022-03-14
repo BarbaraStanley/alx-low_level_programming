20ca8cc6ba8060853fa3c9818ecca5b37ce14ee9
BarbaraStanley <barbeaboo@gmail.com>
2022-03-14 05:23:19 -0700

    Task 7 smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point, print the lowercase alphabet in reverse
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i = 'z';
+
+	while (i > 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
+
