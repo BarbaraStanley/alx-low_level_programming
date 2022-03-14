ea78ada127bff8954646a53c047a2113efa5c2c6
BarbaraStanley <barbeaboo@gmail.com>
2022-03-14 14:09:23 -0700

    Task 8 hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point. a program to print all the numbers of base 16 in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char b;
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+	}
+	for (b = 'a'; b < 'g'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
