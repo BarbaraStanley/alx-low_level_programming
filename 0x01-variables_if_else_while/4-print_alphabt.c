02a65a7f03dfebd8081f7c0c11377416a659b19f
BarbaraStanley <barbeaboo@gmail.com>
2022-03-14 04:02:44 -0700

    Task 4 alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 	char i = 'a';
 
 	while (i <= 'z')
-        {
-		if (i != 'q'&& i != 'e')
+	{
+		if (i != 'q' && i != 'e')
 			putchar(i);
 		i++;
-        }
+	}
 	putchar('\n');
 	return (0);
 }
