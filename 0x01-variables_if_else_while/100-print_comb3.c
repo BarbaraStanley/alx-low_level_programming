63606af729f392cf3536ff388f3016c78b96ded6
BarbaraStanley <barbeaboo@gmail.com>
2022-03-14 16:23:50 -0700

    Task 10 2 digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point, prints all possible combinations of two digit numbers
+ *
+ * Retur: 0
+ */
+int main(void)
+{
+	int b,c,d;
+	b = 0;
+	while (b < 10)
+	{
+		c = b % 10;
+		d = b / 10;
+
+		if (d < c)
+		{
+			putchar(d + '0');
+			putchar(c + '0');
+
+			if (b < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		b++;
+	}
+	putchar('\n');
+		return (0);
+}
