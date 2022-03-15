8a5155f5978efab30e271d1014eae790ff7b719a
BarbaraStanley <barbeaboo@gmail.com>
2022-03-14 17:18:38 -0700

    Task 12 the end

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,16 +13,16 @@ int main(void)
 	{
 		a = i / 10;
 		b = i % 10;
-	
+
 		for (j = 0; j < 100; j++)
 		{
 			c = j / 10;
 			d = j % d;
 
-			if (a < c || (a==c && b < d))
+			if (a < c || (a == c && b < d))
 			{
 				putchar(a + '0');
-				putchar(b +'0');
+				putchar(b + '0');
 				putchar(32);
 				putchar(c + '0');
 				putchar(d + '0');
