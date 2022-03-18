d257ea541f02146b20dcb700a504877755a84e4d
BarbaraStanley <barbeaboo@gmail.com>
2022-03-17 22:15:31 -0700

    Task 11 prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - function that finds and prints the largest prime factor of 612852475143
+ * main - function that finds/prints the largest prime factor of 612852475143
  *
  * Return: 0
  */
