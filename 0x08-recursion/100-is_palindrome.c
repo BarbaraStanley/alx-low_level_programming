e30f4f299e4be4bcd58913926f82063e63a0343b
BarbaraStanley <barbeaboo@gmail.com>
2022-03-30 17:18:45 -0700

    Advanced task 1 recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,24 @@
 
 int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
+int find_strlen(char *s);
 
+/**
+ * find_strlen - Returns the length of a string
+ * @s: The string to be measured
+ * Return: The length of the string
+ */
+
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{								len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
+}
 
 /**
  * check_palindrome - Checks if a string is a palindrome
@@ -32,7 +49,7 @@ int check_palindrome(char *s, int len, int index)
 int is_palindrome(char *s)
 {
 	int index = 0;
-	int len = _strlen_recursion(s);
+	int len = find_strlen(s);
 
 	if (!(*s))
 		return (1);
