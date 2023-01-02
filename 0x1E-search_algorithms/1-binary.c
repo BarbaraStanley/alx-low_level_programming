0f92faff5a9e030aa013fc73bbdf496a7b43b05f
BarbaraStanley <barbeaboo@gmail.com>
2023-01-02 18:43:16 +0000

    search algorithms with c first push

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - function that searches for a value in
+ * a sorted array of integers using the Binary search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ * Return: the index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int l = 0;
+	int a;
+	int r = size - 1;
+	int mid;
+
+	while (l <=  r && array[mid] != value)
+	{
+		mid = (l + r) / 2;
+		printf("Searching in array: ");
+                for (a = l; a < r; a++)
+                {
+                        printf("%d, ", array[a]);
+                }
+                printf("%d\n", array[a]);
+		if (array[mid] == value)
+			return (mid);
+		else if (value > array[mid])
+		{
+			l = mid + 1;
+		}
+		else
+			r = mid - 1;
+	}
+	return (-1);
+}
