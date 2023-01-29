9676144b97d6d75b292f3802a310dd475a72861c
Barbara <barbeaboo@gmail.com>
2023-01-29 10:52:05 +0000

    kernel docs

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head node
+ * Return: number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
